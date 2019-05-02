3cf78d11cbfb1be571a85e179cbc4a7d7053f636
Sean Darsie <v-sedars@microsoft.com>
2019-05-01 21:50:23 -0700

    added a plan in the comments

--- firstDay/PracticeGit.cs
+++ firstDay/PracticeGit.cs
@@ -10,6 +10,10 @@ namespace Practice
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.WriteLine("I am on branch v-sedars, and this is a great day to be me");
+            // ok  now i'm actually going to start coding something. For some reason  i want to code figure out how to code the prefix search feature before anything else so that's what i'm going to do.
+            // For this function i am going to need to have a data structure that holds a list of strings i suppose, and when i type in the console, dynamically create  a list of suggestions that can be shown to the user with some kind of drop down window. 
+            // for now i'm just going to create a function to deal with the strings an print them out whne the user presses 'enter' without trying to display them with an app. 
+            // next i'm going to create an app and add the same funcitonality with a predetermined list of strings, and try to figure out how best to display them. 
         }
     }
 }
